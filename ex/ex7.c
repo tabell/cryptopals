fe5c91370bd8df6a841e97a546dbcac0f2a11a2b
Alex <t.alexander.bell@gmail.com>
2018-12-23 22:03:58 -0800

    refactor

--- ex/ex7.c
+++ ex/ex7.c
@@ -5,6 +5,40 @@
 
 #define errn(str, err, ...) fprintf(stderr, "error %d during %s: %s\n", err, str, strerror(err))
 
+int aes_dec(
+        uint8_t *key,
+        uint8_t key_len,
+        uint8_t *in,
+        uint8_t *out,
+        size_t len)
+{
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+
+    if (!ctx) {
+        fprintf(stderr,"EVP_CIPHER_CTX_init failed\n");
+        return -1;
+    }
+
+    if (1 != (EVP_DecryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL))) {
+        fprintf(stderr,"EVP_DecryptInit_ex failed\n");
+        goto cleanup_ctx;
+    }
+
+    int out_len = 0;
+    if (1 != (EVP_DecryptUpdate(ctx, out, &out_len, in, len))) {
+        fprintf(stderr,"EVP_DecryptUpdate failed\n");
+        goto cleanup_ctx;
+    }
+    if (out_len) fprintf(stderr, "update wrote %d bytes\n", out_len);
+
+    if (1 != EVP_DecryptFinal(ctx, out + out_len, &out_len)) {
+        fprintf(stderr,"EVP_DecrypFinal failed\n");
+    }
+    if (out_len) fprintf(stderr, "final wrote %d bytes\n", out_len);
+
+cleanup_ctx:
+    EVP_CIPHER_CTX_free(ctx);
+}
 int main(int argc, char** argv)
 {
     if (argc < 3) {
@@ -26,7 +60,6 @@ int main(int argc, char** argv)
     size_t key_len;
     if (0 != (ret = file_read_alloc(argv[2], &key, &key_len))) {
         errn("loading ciphertext file", ret);
-        goto done;
     } else {
         dbg("read %lu bytes from keyfile", key_len);
     }
@@ -34,38 +67,14 @@ int main(int argc, char** argv)
     uint8_t *out = malloc(ct_len);
 
     /* Crypto begin */
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-
-    if (!ctx) {
-        fprintf(stderr,"EVP_CIPHER_CTX_init failed\n");
-        goto key_free;
+    if (!ret) {
+        ret = aes_dec(key, key_len, ct, out, ct_len);
+        fprintf(stdout, "%s\n", out);
+    } else {
+        errn("error decoding aes", ret);
     }
 
-    if (1 != (EVP_DecryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL))) {
-        fprintf(stderr,"EVP_DecryptInit_ex failed\n");
-        goto cleanup_ctx;
-    }
-
-    int out_len = 0;
-    if (1 != (EVP_DecryptUpdate(ctx, out, &out_len, ct, ct_len))) {
-        fprintf(stderr,"EVP_DecryptUpdate failed\n");
-        goto cleanup_ctx;
-    }
-    if (out_len) fprintf(stderr, "update wrote %d bytes\n", out_len);
-
-    if (1 != EVP_DecryptFinal(ctx, out + out_len, &out_len)) {
-        fprintf(stderr,"EVP_DecrypFinal failed\n");
-    }
-    if (out_len) fprintf(stderr, "final wrote %d bytes\n", out_len);
-
-    fprintf(stdout, "%s\n", out);
-
-cleanup_ctx:
-    EVP_CIPHER_CTX_free(ctx);
-
-key_free:
     free(key);
-done:
     free(ct);
     free(out);
 }

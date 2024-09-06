0ab11be1b5a962ed9b80fa1cb30ecb999f849e31
ghost <ghost@busters>
2024-09-06 09:18:35 -0700

    working up to ex6

--- src/ex6.c
+++ src/ex6.c
@@ -11,15 +11,15 @@ int main(void)
 {
     uint8_t *buf = NULL;
     int ret;
-    size_t bytes;
-    if (0 != (ret = file_read_alloc(INFILE, &buf, &bytes))) {
+    size_t buflen = 0;
+    if (0 != (ret = file_read_alloc(INFILE, &buf, &buflen))) {
         log("error %d: %s", ret, strerror(-ret));
         return 0;
     }
 
-    size_t keysize = find_keysize(buf, bytes);
+    size_t keysize = find_keysize(buf, buflen);
     dbg("trying keysize %ld", keysize);
-    size_t block_count = bytes / keysize;
+    size_t block_count = buflen / keysize;
     uint8_t *block = NULL;
     char key[keysize + 1];
     key[keysize] = '\0';
@@ -43,8 +43,8 @@ int main(void)
     }
 
     log("key is %s", key);
-    char out[bytes];
-    repeating_xor(buf, out, bytes, key, keysize);
+    char out[buflen];
+    xor(buf, buflen,  key, keysize, out, buflen);
     fprintf(stdout,"%s\n", out);
     free(buf);
     return 0;
